edd269d96c73be0107e8fba58a4d0139348f0bed
ryezzz <ryezzz@gmail.com>
2017-11-30 15:32:55 +0000

    almost final draft ssa

--- workingscript.js
+++ workingscript.js
@@ -1,4 +1,9 @@
-var margin = {top: 10, right: 0, bottom: 0, left: 0},
+var container = d3.select("body")
+               .append("div")
+               .attr('class', 'container')
+               .attr('id', 'countryContainer')
+               
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
     width = window.innerWidth/3 - margin.left - margin.right,
     height = window.innerHeight/3.5- margin.top - margin.bottom;
     
@@ -44,54 +49,50 @@ function render(data, i){
 
         // Sort single country change from most cange to least change (postive and negative combined)
     objectKeys = objectKeys.sort(); // ['apples', 'bananas', 'cherries']
-     
-    // console.log(objectColors[0].Category[0]);
-     
-    //   objectColors.sort(function (a, b) {
-    //     var nameA = a.Category; // ignore upper and lowercase
-    //     var nameB = b.Category; // ignore upper and lowercase
-    //     if (nameA < nameB) {
-    //     return -1;
-    //      }
-    //     if (nameA > nameB) {
-    //     return 1;
-    //      }
-    //       return 0;
-    //     });
-        
- 
-        
+
         
         
     var countryName = data[0]
                     .Country_Name;
-        
-    // var divStackedArea = d3.select('body')
-    //                     .append('div')
-    //                     .attr('class', countryName + "AreaChart")
+   
+
+                       
     
     var chartDiv = d3.select('body')
                  .append('div')
                  .attr('id', 'chartDiv'+ countryName)
                  .attr('class', 'chartDiv');;
     
-     var titleDiv =  chartDiv
-                    .append('svg').attr('class', 'title')
-                    .attr("width", window.innerWidth)
-                    .attr('height', 30)  
+     var titleDiv =  container
+                    .append('div')
+                    .attr('class', 'row')
+                    .append('div')
+                    .attr('class','col-4')
+                    // .append('svg').attr('class', 'title')
+                    // .attr("width", window.innerWidth)
+                    // .attr('height', 30)
                     
-    var svgTogglingTitle =  chartDiv
-                    .append('svg').attr('class', countryName + 'togglingTitle')
-                    .attr("width", window.innerWidth/3)
-                    .attr('height', window.innerWidth/6)
+                    
+     var chartsRow = container
+                    .append('div')
+                    .attr('class', 'row')
+                    
         
+    var togglingTitle =  chartsRow
+                            // .append("div")
+                            // .attr("class","row")
+                            .append('div')
+                            .attr('class','col-4')
+                            .attr('id', "togglingTitle")
 
-    var svgStacked = chartDiv
+    var svgStacked = chartsRow
+                    .append('div')
+                    .attr('class','col-4')
                     .append('svg')
                     .attr('id', "svg" + countryName + "AreaChart")
-                    .attr('width', window.innerWidth/3.5).attr('height', window.innerHeight/3.5),
+                    .attr('width', window.innerWidth/4).attr('height', window.innerHeight/3.5),
                     
-        marginStacked = {top: 30, right: 20, bottom: 30, left: 50},
+        marginStacked = {top: 10, right: 0, bottom: 30, left: 35},
         
         widthStacked = svgStacked.attr("width") - marginStacked.left - marginStacked.right,
         
@@ -114,6 +115,7 @@ function render(data, i){
                 .attr("transform", "translate(" + marginStacked.left + "," + marginStacked.top + ")");
     
     
+    
     xStacked.domain(d3.extent(data, function(d) { return d.Time; }));
     z.domain(objectKeys);
     stack.keys(objectKeys);
@@ -125,69 +127,20 @@ function render(data, i){
 
     layer.append("path")
       .attr("class", function(d,i){
-                    // return countryName + '_' + objectKeys[i] change to target a single country income
-
           return objectKeys[i]
       })
       .style("fill", function(d) { return z(d); })
       .style("stroke",function(d) { return z(d); })
       .attr("d", area);
-
-    layer.filter(function(d) { return d[d.length - 1][1] - d[d.length - 1][0] > 0.01; })
-      .append("text")
-      .attr("x", widthStacked - 6)
-      .attr("y", function(d) { return yStacked((d[d.length - 1][0] + d[d.length - 1][1]) / 2); })
-      .attr("dy", ".35em")
-    
-      .style("font", "10px sans-serif")
-      .style("text-anchor", "end")
       
-      
-            
-            
-    // .on("mouseout", function(d) {		
-    //             tooltip.style("opacity", 0);	
-    //     });
-    
-    //   .text(function(d,i) { return objectKeys[i]; });
-
 // Make min and max of years
     var years = []
 
     for(var i=0; i<data.length; i++){
         years.push(data[i].Time)
     }
-    
 
-
-// //every year for every piece of data with value gets a tick
-// console.log(years)
-
-    gStacked.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + heightStacked + ")")
-    //   .call(d3.axisBottom(xStacked).ticks(data.length).tickValues(years));
-          .call(d3.axisBottom(xStacked).ticks(data.length).tickValues([years[0], d3.max(years)]));
-    gStacked.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(yStacked).ticks(5, "%"));
-  
-
-
-                    // .attr('height', window.innerWidth/6)
-                    
-
-///////////////END STACKED AREA CHART SVG CREATE///////////////////
-        
-    var svg = d3.select('#chartDiv'+ countryName)
-              .append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .attr("class", 'barChartSVG')
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-                    
+     //CREATE CHANGE ARRAY FOR BAR CHART AND TEXT 
     var singleCountryChange = [];
         
     var countryClass = data[0].Country_Name
@@ -215,34 +168,48 @@ function render(data, i){
         allChangeArr.push(changeObj)
     });
 
+    
+///////END CREATE CHANGE ARRAY
 
-    //REDEFINE DATA FOR BAR CHART
-    // data = allChangeArr; 
-    // console.log(data)
+    var format = d3.format(".1f");
+
+
+// //every year for every piece of data with value gets a tick
+
+    gStacked.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + heightStacked + ")")
+    //   .call(d3.axisBottom(xStacked).ticks(data.length).tickValues(years));
+          .call(d3.axisBottom(xStacked).ticks(data.length).tickValues([years[0], years[3], d3.max(years)]));
+    gStacked.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(yStacked).ticks(2, "%"));
+ 
+
+///////////////END STACKED AREA CHART SVG CREATE///////////////////
+        
+    var svg = chartsRow
+              .append('div')
+              .attr('class','col-4')
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .attr("class", 'barChartSVG')
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+
         
 // /////////////////////data cleaned, create Pos NEG Bar Chart///////////////////////////////////////
         
     x.domain([-50, 50]);
     y.domain(allChangeArr.map(function(d) { return d.Category; }));
     
-    
-     var tooltip = d3.select("body")
-                            .append("div")
-                            .style("position", "absolute")
-                            .style("z-index", "10")
-                            .style("visibility", "hidden")
-                            .style("background", "#000")
-                            .text("a simple tooltip");
-                            
-      
     svg.selectAll(".bar")
                 .data(allChangeArr)
                 .enter()
                 .append("rect")
                 .attr("class", function(d,i){
-                    //This is where I target a specific country: classArrayForInteraction.push(countryName + '_' + objectKeys[i])
-                    //return countryName + '_' + objectKeys[i]
-
                     classArrayForInteraction.push(objectKeys[i])
                     return objectKeys[i]
                     })                    
@@ -263,11 +230,50 @@ function render(data, i){
             		         }
             			}    
     		    	})
-				        
+ function increasedDecreased(positiveneg){ 
+     if(positiveneg<0){
+         return "decreased"
+     } else {return "increased"};
+ }
+ 
+ function parseClass(objectkey){ 
+     return objectkey.replace('aIncome', "Income")
+                 .replace('bIncome', "Income")
+                 .replace('cIncome', "Income")
+                 .replace('dIncome', "Income")
+                 .replace('eIncome', "Income")
+                 .replace('twenty', " quintile ")
+                 .replace('by', " by the ")
+                 .replace(/_/g, ' ')}    		    	
+////Interactivity			        
+togglingTitle
+      .selectAll ('text')
+      .data(allChangeArr)
+      .enter()
+      .append("text")
+      .attr('class', function(d, i){return classArrayForInteraction[i]+"text"})
+    //   .style("font", "10px sans-serif")
+    //   .style("text-anchor", "end")
+      
+      .text(function(d, i) {
+          var thisText = parseClass(objectKeys[i]) + increasedDecreased(allChangeArr[i].Change*1) + " from " + format(data[0][objectKeys[i]]*100) + " to " + format(data[years.length-1][objectKeys[i]]*100) + " percent" + ", a " + format(allChangeArr[i].Change) + " percent" +" change.";
+          return thisText;
 
-// console.log(classArrayForInteraction);
+      })
+      
+    //   .style("fill", function(d) {  
+    //                     for(var i=0; i<objectColors.length; i++){
+    //         		         if(d.Category === objectColors[i].Category){
+    //         		            return objectColors[i].color ;
+    //         		         }
+    //         			}    
+    // 		    	})
+    
+      .style("font-size", 0)
+          .style("fill", "red")
 
-console.log(classArrayForInteraction);
+
+
 
   	svg.selectAll(".change")
 					.data(allChangeArr)
@@ -282,6 +288,7 @@ console.log(classArrayForInteraction);
 						 })
 						
 					 .attr("y", function(d){ return y(d.Category); })
+					 //change space inbetween bars
 					 .attr("dy", y.bandwidth() - 2.55)
 					 .attr("text-anchor", function(d){
 							if (d.Change < 0){
@@ -297,11 +304,12 @@ console.log(classArrayForInteraction);
 								return (x(d.Change) - x(0)) > 20 ? "#fff" : "#3a403d";
 							}
 						})
-						
+						.attr("text-anchor", "end")
+				        // .text(function(d){ return format(d.Change)+"%"; });
 				// 	  .text(function(d){ return d.Change; });
                        
     
-    
+                    
                     svg.selectAll(".category")
 						.data(allChangeArr)
 					    .enter()
@@ -309,9 +317,10 @@ console.log(classArrayForInteraction);
 						.attr("class", "category")
 						.attr("x", function(d){ return d.Change < 0 ? x(0) + 2.55 : x(0) - 2.55 })
 						.attr("y", function(d){ return y(d.Category); })
+						//change height between bars
 						.attr("dy", y.bandwidth() - 2.55)
-						.attr("text-anchor", function(d){ return d.Change < 0 ? "start" : "end"; })
-				        .text(function(d){ return d.Category; });
+						.attr("text-anchor", "end")
+				        // .text(function(d){ return format(d.Change)+"%"; });
 						
 						
 					svg.append("line")
@@ -325,89 +334,56 @@ console.log(classArrayForInteraction);
 // console.log(unParsedData)
 ///create main title content
 
-        titleDiv
-        .append('text')
-        .text(function(){return "Income quintile distribution in " + countryName.replace('_', ' ') + " from " + d3.min(years) + " to " + d3.max(years)})
-        .attr('alignment-baseline','hanging');
-
-
-        // svgTogglingTitle 
+        // togglingTitle
         // .append('text')
-        // // .text(function(){return years})
-        // .attr('alignment-baseline','hanging');
-//   var tooltip = d3.select('body').append("div")	
-//     .attr("class", "tooltip")				
-//     .style("opacity", 0);
-    
-    
-//     d3.selectAll('rect').on("mouseover", function(d) {		
-//             tooltip.text("rect")
-//             .style("opacity", .9);
-//                 // .style("left", (d3.event.pageX) + "px")		
-//                 // .style("top", (d3.event.pageY - 28) + "px");	
+        // .attr('class', 'staticTitle')
+        // .text(function(){return "Income quintile distribution in " + countryName.replace('_', ' ') + " from " + d3.min(years) + " to " + d3.max(years)})
+        // .attr('alignment-baseline','hanging')
 
-
-console.log (data)
-console.log(allChangeArr)
-//             })
-var togglingTitleClass = '.' + countryName + 'togglingTitle';
 
     function useClassForInteraction (){
         
        for(var i = 0; i < classArrayForInteraction.length; i++){
            
-           
-           
                  d3.selectAll('.'+ classArrayForInteraction[i])
                  
                  .on("mouseover", function(){
                   //this returns change info for tanzania  
-                     for(var i =0; i<allChangeArr.length; i++){
-                         if(allChangeArr[i].Category==this.className.baseVal){
+                    //  for(var i =0; i<allChangeArr.length; i++){
+                    //      if(allChangeArr[i].Category==this.className.baseVal){
   
-                             console.log(allChangeArr[i].Change)
-                         }
-                     }
+                    //          console.log(allChangeArr[i].Change)
+                    //      }
+                    //  }
 
                      d3.select( "." + countryName + 'togglingTitle')
-                     
-                     .append('text')
-                     
-                     .text(this.className.baseVal
-                            .replace('aIncome', "Income")
-                            .replace('bIncome', "Income")
-                            .replace('cIncome', "Income")
-                            .replace('dIncome', "Income")
-                            .replace('eIncome', "Income")
-                            .replace(/_/g, ' ')
-                            + data[0].dIncome_share_held_by_fourth_twenty*100
-                            +" " + allChangeArr[0].Change
-                     
-                              
                     
-                            )
                      .attr('alignment-baseline','hanging')
-                    //  .append('text')
-                    //  .text(data[0].dIncome_share_held_by_fourth_twenty);
-                     
 
-                     
                     for(var j = 0; j < classArrayForInteraction.length; j++){
                             if(classArrayForInteraction[j] != this.className.baseVal){
+                            d3.selectAll('.'+classArrayForInteraction[j]+"text").style("font-size", 0)    .style("fill", "red")
+; 
                             d3.selectAll('.'+classArrayForInteraction[j]).transition()
-                            .duration(300).style('opacity', 0.1 )
-                            }
+                            .duration(200).style('opacity', 0.1 )
+                            // .text("hello")
+                            } else { d3.selectAll('.'+classArrayForInteraction[j]+"text").style("font-size", 20)} 
                      }
                      
                      
                      
         })
                 .on("mouseout", function(){ 
-                    d3.select( "." + countryName + 'togglingTitle')
-                    .selectAll("text").remove();
+                    d3.selectAll('svg')
+                    // .selectAll("text").remove();
                     for(var j = 0; j < classArrayForInteraction.length; j++){
-                         d3.selectAll('.'+classArrayForInteraction[j]).transition()
-                            .duration(300).style('opacity', 1)
+                        d3.selectAll('.'+classArrayForInteraction[j]+"text").style("font-size", 0)
+                         d3.selectAll('.'+classArrayForInteraction[j]).style("font-size", 0)
+                         
+                            d3.selectAll('.'+classArrayForInteraction[j])
+                            .transition()
+                            .duration(200)
+                            .style('opacity', 1)
                      }
 
                  })
@@ -417,10 +393,14 @@ var togglingTitleClass = '.' + countryName + 'togglingTitle';
         }
 
     };
-    
+    //Income quintile distribution 
     useClassForInteraction()
     
-        
+    titleDiv
+        .append('text')
+        .attr('class', 'staticTitle')
+        .text(function(){return countryName.replace('_', ' ') + " from " + d3.min(years) + " to " + d3.max(years)})
+        .attr('alignment-baseline','hanging')
     }
     
     
@@ -488,8 +468,8 @@ var togglingTitleClass = '.' + countryName + 'togglingTitle';
         //     .attr('class', 'key')
         //     .append('rect')
             // .attr('color', function(d) { return z(d.length)})
-        render(country1)
         render(country2)
+        render(country1)
         render(country3)
        
        
